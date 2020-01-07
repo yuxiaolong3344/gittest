763e133a2fba39847c09c06cd1807c5e281c150e
yuxiaolong7120 <yuxiaolong7120@163.com>
2020-01-07 19:40:04 +0800

    03

--- src/main/java/com/yxl/HW.java
+++ src/main/java/com/yxl/HW.java
@@ -4,16 +4,6 @@ public class HW {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello wordgggg gggggg");
-        System.out.println("hello wordgggg gggggg");
-        System.out.println("hello wordgggg gggggg");
-        System.out.println("hello wordgggg gggggg");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg ttttt");
-        System.out.println("hello wordgggg aaa");
+        System.out.println("    ");
     }
 }
